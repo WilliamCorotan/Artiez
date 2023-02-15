75eacabc30718adc69b8dd466cd9609a2663de66
William Corotan <williamcorotan@gmail.com>
2023-02-16 02:57:34 +0800

    Modified routes for admin

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/artist/dashboard', function () {
     return Inertia::render('Artist/Home');
-});
+})->middleware(['auth', 'verified']);
 Route::get('/artworks/add', [ArtworkController::class, 'create']);
 
 require __DIR__ . '/auth.php';
