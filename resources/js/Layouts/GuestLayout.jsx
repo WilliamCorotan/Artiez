19754fe7f73d6ebc32879514555cca594ce0f932
RTDeluna <ramueldeluna30@gmail.com>
2023-02-18 19:58:52 +0800

    fixing error

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,11 +1,9 @@
-
 import ApplicationLogo from "@/Components/ApplicationLogo";
 
 import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
-
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <Link to="/">
@@ -17,14 +15,13 @@ export default function Guest({ children }) {
                 </Link>
             </div>
 
-
             {/* <div className="w-2/3 min-h-screen flex place-items-center mx-auto">
                 <div className="mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg"> */}
             {children}
             {/* </div>
             </div> */}
             {/* </div> */}
-        </>
+        </div>
     );
 }
 
