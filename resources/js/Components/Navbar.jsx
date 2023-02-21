8a3ad1f12105c3bf19458a43d49570b60065014a
William Corotan <williamcorotan@gmail.com>
2023-02-21 16:19:45 +0800

    Profile edit pages done

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -14,15 +14,15 @@ const Navbar = ({ auth }) => {
         "items-center justify-between hidden w-full md:flex md:w-auto md:order-1";
     return (
         <>
-            <nav className="bg-white border-rose-600 px-2 sm:px-4 py-2.5 rounded ">
+            <nav className="bg-white border-b border-rose-600 px-2 sm:px-4 py-2.5 rounded ">
                 {/* Parent Container */}
                 <div className="w-full mx-auto flex flex-wrap items-center justify-between ">
                     {/* Logo */}
                     <div className="mx-8">
-                        <Link href="/" className="flex items-center">
+                        <Link href="/" className="h-full flex items-center">
                             <img
-                                src="assets/artiez logo.svg"
-                                className="h-10 mr-1 sm:h-9"
+                                src="./assets/artiez-full.svg"
+                                className="h-20 mr-1 "
                                 alt="Artiez Logo"
                             />
                         </Link>
@@ -48,7 +48,7 @@ const Navbar = ({ auth }) => {
                             </div>
                             <TextInput
                                 type="text"
-                                className="p-2 pl-10 pr-32 text-sm text-gray-900 border border-rose-600 rounded-lg bg-gray-50 focus:ring-rose-900 focus:border-rose-600"
+                                className="p-2 pl-10 pr-32 text-sm text-gray-900 border border-rose-600 rounded-sm bg-gray-50 focus:ring-rose-900 focus:border-rose-600"
                                 placeholder="Search"
                             />
                         </div>
--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import DeleteUserForm from "./Partials/DeleteUserForm";
 import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
+import UpdateAddressForm from "./Partials/UpdateAddressForm";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm";
 import Layout from "@/Layouts/Layout";
 
@@ -173,6 +174,14 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
                     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                         <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                             <UpdateProfileInformationForm
+                                mustVerifyEmail={mustVerifyEmail}
+                                status={status}
+                                className="max-w-xl"
+                            />
+                        </div>
+
+                        <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                            <UpdateAddressForm
                                 mustVerifyEmail={mustVerifyEmail}
                                 status={status}
                                 className="max-w-xl"
